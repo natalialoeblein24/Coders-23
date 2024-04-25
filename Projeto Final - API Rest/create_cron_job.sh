409f6d4e0a12d54e54d045037aa8e5d7a01c0bb5
Natalia Loeblein <122561383+natalialoeblein24@users.noreply.github.com>
2024-04-25 01:03:18 -0300

    Delete Projeto Final - API Rest directory

--- Projeto Final - API Rest/create_cron_job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-crontab -l > ~/API Rest - Sistema de Monitoramento de Notícias sobre Criptomoeda/mycron
-echo "0 * * * * ~/API Rest - Sistema de Monitoramento de Notícias sobre Criptomoeda/cron.sh" >> ~/API Rest - Sistema de Monitoramento de Notícias sobre Criptomoeda/mycron
-
-crontab ~/API Rest - Sistema de Monitoramento de Notícias sobre Criptomoeda/mycron
-rm mycron
-echo feito
--- Projeto Final - API Rest/cron.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python ~/API Rest - Sistema de Monitoramento de Notícias sobre Criptomoeda/requisicao.py
-python ~/API Rest - Sistema de Monitoramento de Notícias sobre Criptomoeda/selects.py
-echo run_time: $(date +"%D_%T") >> ~/API Rest - Sistema de Monitoramento de Notícias sobre Criptomoeda/log.txt
